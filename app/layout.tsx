9681ff774ecccd3a137ea2b697cc7407a2bee738
花木 登 <hanakinoboru@HanakinMacmini2024.local>
2026-02-08 23:10:41 +0900

    Meta Pixel（Facebook広告）を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import type { Metadata, Viewport } from 'next'
+import Script from 'next/script'
 import { M_PLUS_Rounded_1c } from 'next/font/google'
 
 import { Toaster } from '@/components/ui/sonner'
@@ -31,6 +32,34 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${mplus.variable} font-sans antialiased`}>
+        {/* Meta Pixel Code - head相当で読み込み（beforeInteractiveで初期HTMLに含まれる） */}
+        <Script
+          id="meta-pixel"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1265449258783286');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1265449258783286&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <Toaster richColors position="top-center" />
       </body>
